8971e39144d311b203bd7ff95c42ee0d2f9af9aa
Timo Goltz <162825846+Timo1002@users.noreply.github.com>
2025-08-07 15:56:52 +0200

    updated

--- api/sources/11880.ts
+++ api/sources/11880.ts
@@ -42,7 +42,23 @@ export async function search11880(query: string): Promise<SourceResult> {
   try {
     console.log('🔍 11880: Starting NEW search for:', query);
     
-    browser = await chromium.launch({ headless: true });
+    browser = await chromium.launch({ 
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu',
+        '--disable-extensions',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding'
+      ]
+    });
     const page = await browser.newPage();
     
     await page.setExtraHTTPHeaders({
